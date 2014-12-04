c1418c0b3c856e58a44e12bd85112d3cdf68c452
vkz <vladilen.kozin@gmail.com>
2014-12-04 21:09:48 +0300

    Sample mocha test. Repetitive suply of templates (> 2 chunks) seems to work: see scratch.js for a trivial example.

--- test/generate-test.js
+++ test/generate-test.js
@@ -2,32 +2,35 @@ var more = require('..');
 var assert = require('assert');
 var bemxjst = require("bem-xjst");
 
+
+
 describe('Client templating', function() {
 
   function test(fn1, fn2, options) {
     if (!options) options = {};
-    var templates = fn2.toString().replace(/^function\s*\(\)\s*{|}$/g, '');
-    var moreTemplates = fn1.toString().replace(/^function\s*\(\)\s*{|}$/g, '');
-    var result1 = more.generate(templates);
-    var result = more.generateMore(moreTemplates, result1.ir);
+    var templates = fn1.toString().replace(/^function\s*\(\)\s*{|}$/g, '');
+    var moreTemplates = fn2.toString().replace(/^function\s*\(\)\s*{|}$/g, '');
+    var compiler = new more.Compiler();
+    var result1 = compiler.generate(templates);
+    var result = compiler.generateMore(moreTemplates, result1.ir);
+    var bemxjstResult = bemxjst.generate(templates + '\n' + moreTemplates, options);
     assert.equal(
       result.out,
-      bemxjst.generate(templates + '\n' + moreTemplates, options));
+      bemxjstResult);
   }
 
-  it('Should produce IR for templates', function () {
-    assert.doesNotThrow(function () {
-      more.generate(function () {
-        block('attach').elem('control')(
-          tag()('input'),
-          attrs()(function() {
-            return { type : 'file' };
-          })
-        );
-        block('attach').elem('no-file').tag()('span');
-        block('attach').elem('file').tag()('span');
-        block('attach').elem('text').tag()('span');
-      })});
+  it('Should generate the same result as bem-xjst on a single chunk of templates', function () {
+    test(function () {
+      block('attach').elem('control')(
+        tag()('input'),
+        attrs()(function() {
+          return { type : 'file' };
+        })
+      );
+      block('attach').elem('no-file').tag()('span');
+      block('attach').elem('file').tag()('span');
+      block('attach').elem('text').tag()('span');
+    }, function () {})
   });
 
   it('Should load a simple template', function() {
